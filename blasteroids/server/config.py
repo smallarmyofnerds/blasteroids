6cde6d2626b05de5b2aef10abe54fa27fc906978
Jamie Hale <jamiesonhale@gmail.com>
2021-09-04 17:19:25 -0400

    Tidy config

--- blasteroids/server/config.py
+++ blasteroids/server/config.py
@@ -1,54 +1,4 @@
 import configparser
-import socket
-
-DEFAULT_SERVER_ADDRESS = ''
-DEFAULT_SERVER_PORT = 19999
-
-DEFAULT_SHIP_ACCELERATION_RATE = 1
-DEFAULT_SHIP_ROTATIONAL_ACCELERATION_RATE = 2
-DEFAULT_SHIP_ROTATIONAL_VELOCITY_FRICTION = 0.1
-DEFAULT_SHIP_LINEAR_FRICTION = 0.1
-DEFAULT_SHIP_RADIUS = 20
-DEFAULT_SHIP_DAMAGE = 100
-DEFAULT_SHIP_HEALTH = 100
-DEFAULT_SHIP_MAX_SHIELDS = 1000
-
-DEFAULT_LASER_SPEED = 1000
-DEFAULT_LASER_RADIUS = 5
-DEFAULT_LASER_DAMAGE = 200
-DEFAULT_LASER_LIFESPAN = 1000
-DEFAULT_LASER_COOLDOWN = 200
-
-DEFAULT_RAPID_FIRE_SPEED = 1500
-DEFAULT_RAPID_FIRE_RADIUS = 2
-DEFAULT_RAPID_FIRE_DAMAGE = 80
-DEFAULT_RAPID_FIRE_LIFESPAN = 1000
-DEFAULT_RAPID_FIRE_COOLDOWN = 80
-
-DEFAULT_MIN_ASTEROIDS = 40
-DEFAULT_ASTEROID_MAX_SPEED = 50
-DEFAULT_ASTEROID_BASE_DAMAGE = 800
-DEFAULT_ASTEROID_BASE_HEALTH = 200
-DEFAULT_ASTEROID_BASE_COLLISION_RADIUS = 12
-
-DEFAULT_HEART_HEALTH = 400
-DEFAULT_SHIELD_AMOUNT = 400
-
-DEFAULT_DOUBLE_FIRE_LIFESPAN = 6000
-DEFAULT_HEART_LIFESPAN = 6000
-DEFAULT_MEGA_HEART_LIFESPAN = 3000
-DEFAULT_MEGA_SHIELD_LIFESPAN = 3000
-DEFAULT_PROXIMITY_MINE_LIFESPAN = 3000
-DEFAULT_RAPID_FIRE_LIFESPAN = 6000
-DEFAULT_ROCKET_SALVO_LIFESPAN = 3000
-DEFAULT_ROCKET_LIFESPAN = 3000
-DEFAULT_SHIELD_LIFESPAN = 6000
-DEFAULT_SPREAD_FIRE_LIFESPAN = 6000
-DEFAULT_TIME_BOMB_LIFESPAN = 3000
-
-DEFAULT_WORLD_WIDTH = 10000
-DEFAULT_WORLD_HEIGHT = 10000
-DEFAULT_WORLD_EDGE_ACCELERATION_FACTOR = 2
 
 
 class ServerConfig:
@@ -56,9 +6,11 @@ class ServerConfig:
         self.address = config['Address']
         self.port = int(config['Port'])
 
+
 class MiscConfig:
     def __init__(self, config):
         self.logging_level = config['LoggingLevel']
+
 
 class WorldConfig:
     def __init__(self, config):
@@ -66,6 +18,8 @@ class WorldConfig:
         self.height = int(config['Height'])
         self.edge_acceleration_factor = float(config['EdgeAccelerationFactor'])
         self.min_obstacles = int(config['MinObstacles'])
+        self.safe_respawn_distance = int(config['SafeRespawnDistance'])
+
 
 class ShipConfig:
     def __init__(self, config):
@@ -78,6 +32,7 @@ class ShipConfig:
         self.max_health = int(config['MaxHealth'])
         self.max_shields = int(config['MaxShields'])
 
+
 class LaserConfig:
     def __init__(self, config):
         self.projectile_speed = int(config['ProjectileSpeed'])
@@ -85,6 +40,7 @@ class LaserConfig:
         self.projectile_damage = int(config['ProjectileDamage'])
         self.projectile_lifespan = int(config['ProjectileLifespan'])
         self.cooldown = int(config['Cooldown'])
+
 
 class RapidFireConfig:
     def __init__(self, config):
@@ -95,33 +51,40 @@ class RapidFireConfig:
         self.projectile_lifespan = int(config['ProjectileLifespan'])
         self.cooldown = int(config['Cooldown'])
 
+
 class DoubleFireConfig:
     def __init__(self, config):
         self.pickup_lifespan = int(config['PickupLifespan'])
         self.offset = int(config['Offset'])
+
 
 class SpreadFireConfig:
     def __init__(self, config):
         self.pickup_lifespan = int(config['PickupLifespan'])
         self.spread = int(config['Spread'])
 
+
 class HeartConfig:
     def __init__(self, config):
         self.pickup_lifespan = int(config['PickupLifespan'])
         self.health_amount = int(config['HealthAmount'])
 
+
 class MegaHeartConfig:
     def __init__(self, config):
         self.pickup_lifespan = int(config['PickupLifespan'])
+
 
 class ShieldConfig:
     def __init__(self, config):
         self.pickup_lifespan = int(config['PickupLifespan'])
         self.shield_amount = int(config['ShieldAmount'])
 
+
 class MegaShieldConfig:
     def __init__(self, config):
         self.pickup_lifespan = int(config['PickupLifespan'])
+
 
 class AsteroidConfig:
     def __init__(self, config):
@@ -130,6 +93,7 @@ class AsteroidConfig:
         self.base_damage = int(config['BaseDamage'])
         self.base_health = int(config['BaseHealth'])
         self.base_radius = int(config['BaseRadius'])
+
 
 class ProximityMineConfig:
     def __init__(self, config):
@@ -141,6 +105,7 @@ class ProximityMineConfig:
         self.explosion_radius = int(config['ExplosionRadius'])
         self.explosion_damage = int(config['ExplosionDamage'])
 
+
 class TimeBombConfig:
     def __init__(self, config):
         self.pickup_lifespan = int(config['PickupLifespan'])
@@ -150,6 +115,7 @@ class TimeBombConfig:
         self.explosion_radius = int(config['ExplosionRadius'])
         self.explosion_damage = int(config['ExplosionDamage'])
 
+
 class RocketConfig:
     def __init__(self, config):
         self.pickup_lifespan = int(config['PickupLifespan'])
@@ -157,6 +123,7 @@ class RocketConfig:
         self.projectile_radius = int(config['ProjectileRadius'])
         self.projectile_damage = int(config['ProjectileDamage'])
         self.projectile_lifespan = int(config['ProjectileLifespan'])
+
 
 class RocketSalvoConfig:
     def __init__(self, config):
@@ -167,6 +134,7 @@ class RocketSalvoConfig:
         self.projectile_lifespan = int(config['ProjectileLifespan'])
         self.spread = int(config['Spread'])
 
+
 class LootConfig:
     def __init__(self, config):
         self.level_1_chance = int(config['Level1Chance'])
@@ -175,6 +143,7 @@ class LootConfig:
         self.level_2_drops = list(filter(lambda d: len(d) > 0, config['Level2Drops'].split(',')))
         self.level_3_chance = int(config['Level3Chance'])
         self.level_3_drops = list(filter(lambda d: len(d) > 0, config['Level3Drops'].split(',')))
+
 
 class Config:
     def __init__(self, filename):
